cbd6e5d4536a9f40faa338495d489ab882e877a7
Rana Waqas <rana.waqas@codeninjaconsulting.com>
2024-04-24 17:14:50 +0500

    changes

--- src/app/api/analyze/route.ts
+++ src/app/api/analyze/route.ts
@@ -2,24 +2,19 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 import { scrapeWebsiteDetails } from "./srapperPuppeteer";
 
-export async function POST(req: NextApiRequest , res: NextApiResponse | any) {
+export async function POST(req: NextApiRequest, res: NextApiResponse | any) {
   const jsonReq = await req.json();
   const url = jsonReq.url;
   console.log({ url });
- 
+
   if (!url) {
-    return  NextResponse.json({ message: "url is required" })
     console.log({ url })
+    return NextResponse.json({ message: "url is required" })
   }
   try {
     const data = await scrapeWebsiteDetails(url);
-
-  
-
-    return  NextResponse.json({ data, message: "success", status: 200 });
-
+    return NextResponse.json({ data, message: "success", status: 200 });
   } catch (error) {
-
     NextResponse.json({ error: "success", status: 500 });
     console.error('Failed to analyze page:', error);
   }
