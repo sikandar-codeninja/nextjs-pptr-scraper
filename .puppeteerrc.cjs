eb5caac95d5820a9fcb9032b6e731d2be3842e48
Sikandar Ramzan <sikandar.ramzan@codeninjaconsulting.com>
2024-07-20 03:45:34 +0500

    logging dirnamers

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,5 +1,8 @@
 const { join } = require("path");
 
+console.log("__dirnameofCjs", __dirname);
+console.log("dirname for .cache", join(__dirname, ".cache", "puppeteer"));
+
 /**
  * @type {import("puppeteer").Configuration}
  */
